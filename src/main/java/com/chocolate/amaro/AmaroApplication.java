e26be881c2739c00138e4efdef6e5217d46a53c6
Abel3581 <marzoa3581@gmail.com>
2022-01-22 21:23:25 -0300

    first commit

---
+++ src/main/java/com/chocolate/amaro/AmaroApplication.java
@@ -0,0 +1,13 @@
+package com.chocolate.amaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmaroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chocolate/amaro/AmaroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chocolate.amaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
