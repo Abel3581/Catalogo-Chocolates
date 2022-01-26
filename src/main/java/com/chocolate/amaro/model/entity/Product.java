08fbe7550d70119867b63ac4cae140ccf250f80e
Abel3581 <marzoa3581@gmail.com>
2022-01-25 23:07:43 -0300

    Add entity Product and Category Maped

---
+++ src/main/java/com/chocolate/amaro/model/entity/Product.java
@@ -0,0 +1,37 @@
+package com.chocolate.amaro.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "products_id")
+    private long id;
+    
+    @Column(nullable = false)
+    private String name;
+    
+    @Column(nullable = false)
+    private String description;
+    
+    @Column(nullable = false)
+    private String image;
+    
+    @Column(name = "soft_delete")
+    private boolean softDelete;
+
+    @JoinColumn(name = "categories_id")
+    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    private Category category;
+}
